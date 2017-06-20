d3d3e0eeaf696e2b50e4972eca7930c946a416e0
Frank Berdele <f.berdel@exinit.de>
2017-06-20 12:11:08 +0200

    recordbook js changes

---
+++ Resources/Public/js/recordbook.js
@@ -0,0 +1,88 @@
+/**
+ * Created by exinit on 19.06.2017.
+ */
+
+var recordCount = 0;
+
+
+function doNothing(){
+  alert('Do Nothing');
+  if(recordCount==0){
+    document.getElementById('addRecordButton').setAttribute('onclick', 'addRecord()');
+  }
+
+}
+
+function addRecord(){
+
+  if(recordCount == 0){
+    document.getElementById('end').innerHTML =
+      '<div class="row" id="newRecordDiv'+ recordCount + '">' +
+        '<div class="row">' +
+          '<div class="col-md-1">' +
+            '<button id="addRecordButton' + recordCount +'" class="form-control" value="" type="button"  onclick="addRecord();">' +recordCount + '</button>' +
+          '</div>' +
+          '<div class="col-md-1">' +
+            '<button id="delRecordButton' + recordCount +'" class="form-control" value="-" type="button" onclick="delRecord(this.id);">-</button>' +
+          '</div>'+
+          '<div class="col-md-3">' +
+            '<input class="form-control" type="text" value="Berichtshefteintrag">' +
+          '</div> ' +
+          '<div class="col-md-1"> ' +
+            '<input class="form-control" type="text" value="00:h 00:m">' +
+          '</div> ' +
+          '<div class="col-md-4">' +
+            '<input class="form-control" type="text" value="Kommentare zum Eintrag">' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+      '<div id="end' + recordCount +'"></div>';
+
+
+      //document.getElementById('addRecordButton').setAttribute('onclick', 'doNothing()');
+
+
+
+  }
+  else{
+    document.getElementById('end'+ (recordCount-1)).innerHTML =
+      '<div class="row" id="newRecordDiv'+ recordCount + '">' +
+        '<div class="row">' +
+          '<div class="col-md-1">' +
+            '<button id="addRecordButton' + recordCount +'" class="form-control" value="" type="button" onclick="addRecord();">' + recordCount +'</button>' +
+          '</div>' +
+          '<div class="col-md-1">' +
+            '<button id="delRecordButton' + recordCount +'" class="form-control" value="delRecordButton' + recordCount + '" type="button" onclick="delRecord(this.id);">-</button>' +
+          '</div>' +
+      '<div class="col-md-3">' +
+      '<input class="form-control" type="text" value="Berichtshefteintrag">' +
+      '</div> ' +
+      '<div class="col-md-1"> ' +
+      '<input class="form-control" type="text" value="00:h 00:m">' +
+      '</div> ' +
+      '<div class="col-md-4">' +
+      '<input class="form-control" type="text" value="Kommentare zum Eintrag">' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+      '<div id="end' + recordCount +'"></div>';
+
+    if(recordCount>0){
+     // document.getElementById('addRecordButton' + (recordCount-1)).setAttribute('onclick', 'doNothing()');
+    }
+
+  }
+
+  return recordCount++;
+}
+
+
+function delRecord(clicked_id){
+  var id = document.getElementById(clicked_id).id;
+  id = id.slice(15, 16);
+    var record = document.getElementById('newRecordDiv' + id);
+    record.parentNode.removeChild(record);
+
+
+    return recordCount--;
+}
