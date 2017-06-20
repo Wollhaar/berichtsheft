55d0fc8a469dc227555802e5c3dbe664568e1ddb
David Goraj <d.goraj@exinit.de>
2017-06-20 12:15:26 +0200

    [task] merged projects: added david/DB_Connection.php to project and changed serversettings

---
+++ Resources/PHP/model/DBManager/DB_Connection.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class DB_Connection extends PDO
+{
+    protected $host = 'localhost';
+
+    protected $user = 'frankb_7';
+
+    protected $password = 'eB7NBTH1Xkt7Tamc';
+
+    protected $options = array('db_name' => 'recordbook');
+
+    private $dbc = NULL;
+
+    public function __construct()
+    {
+        $this->dbc = $this->getConnection();
+        if (is_a($this->dbc, 'PDO')) {
+            return $this->dbc;
+        }
+    }
+
+    public function getConnection()
+    {
+        if (!isset($this->dbc) || $this->dbc === NULL) {
+            $this->dbc = $this->createConnection();
+            if ($this->dbc === false || $this->dbc === NULL) {
+                return 'please try again later';
+            }
+        }
+        if (isset($this->dbc) && is_a($this->dbc, get_parent_class($this))) {
+            return $this->dbc;
+        }
+    }
+
+    public function createConnection(){
+        try {
+            if (isset($this->dbc)) {
+                return $this->dbc;
+            } elseif ($this->dbc === NULL || !isset($this->dbc)) {
+                $this->dbc = new PDO("mysql:dbname=davidz_report;host=localhost", $this->user, $this->password);
+                return $this->dbc;
+            }
+
+        }
+        catch (Exception $e){
+            die($e->getMessage());
+        }
+
+        return false;
+
+    }
+}
