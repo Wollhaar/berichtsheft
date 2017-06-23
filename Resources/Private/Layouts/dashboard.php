17353fe41e48c40b17e9117bdac12328152f6f96
David Goraj <d.goraj@exinit.de>
2017-06-23 14:12:49 +0200

    [task] edited dashboard.php for logged user

--- Resources/Private/Layouts/dashboard.php
+++ Resources/Private/Layouts/dashboard.php
@@ -1,3 +1,17 @@
+<?php
+
+if (isset($_REQUEST['PHPSESSID']) || isset($_SESSION['session_id'])) {
+    if (isset($_REQUEST['PHPSESSID'])) {
+        $session_id = $_REQUEST['PHPSESSID'];
+    } elseif (isset($_SESSION['session_id'])) {
+        $session_id = $_SESSION['session_id'];
+    }
+    session_start($session_id);
+} else {
+    session_start();
+}
+$_SESSION['session_id'] = $_REQUEST['PHPSESSID'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,7 +31,7 @@
     <link href="/Berichtsheft/Resources/Public/css/main.css" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="Berichtsheft/Resources/Public/js/bootstrap.min.js"></script>
+    <script src="/Berichtsheft/Resources/Public/js/bootstrap.min.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -46,7 +60,7 @@
                 </li>
             </ul>
             <div class="user-field">
-                <span class="username">Eingeloggt als <a href="/Berichtsheft/index.php?case=logged" class="dropdown-toggle" data-toggle="dropdown"><?php echo $login['username']; ?></a></span>
+                <span class="username">Eingeloggt als <a href="/Berichtsheft/index.php?case=logged" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['user']['username']; ?></a></span>
                 <ul class="dropdown-menu">
                     <li><a href="/Berichtsheft/index.php?case=profile"></a>Profil</li>
                     <li><a href="/Berichtsheft/index.php?case=status">Status</a></li>
@@ -60,12 +74,9 @@
 <div class="main">
     <div class="board">
         <?php if($_SESSION['user']['check'] === true): ?>
-        <div class="welcome">
-            <h2 style="color: #00ff00">Sie wurden erfolgreich registriert.<br/>Willkommen im Berichtstoolheft!</h2>
-            <p style="color: lime">Wir wünschen eine erfolgreiche Ausbildung.</p>
-        </div>
+        <?php include_once '/Berichtsheft/Resources/Private/Partial/welcome.html'; ?>
         <?php endif; ?>
-        <?php var_dump($_SESSION); ?>
+<!--        --><?php //var_dump($_SESSION); ?>
         <div class="dashboard">
             <h3>Dein Dashboard</h3>
             <p>Alle relevanten Dinge sind, hier für dich notiert.</p>
