b02fe5260d83653245702d8dac6134b47e30e821
Frank Berdele <f.berdel@exinit.de>
2017-06-27 14:25:34 +0200

    anpassung der restoreTabel funktion an die aktuellen Tabellenstruktur der DB

--- Resources/Private/Layouts/recordbook.php
+++ Resources/Private/Layouts/recordbook.php
@@ -12,9 +12,11 @@ ini_set('display_errors', 'On');
 require_once('../../PHP/model/DBManager/DB_Record.php');
 $recordDBConection = new DB_Record();
 
+$recordDBConection->writeRecordbookDummy();
+
 session_start();
 
-$_SESSION['recordMonth'] = $recordDBConection->getRecordMonth(2017, 06);
+//$_SESSION['recordMonth'] = $recordDBConection->getRecordMonth(2017, 06);
 
 include('html/recordbook.html');
 
