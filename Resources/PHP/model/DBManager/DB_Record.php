0eba65068ff3cae40a5ef09cacb9370c2f788ff1
David Goraj <d.goraj@exinit.de>
2017-06-29 14:49:32 +0200

    [task] edited DB_Record.php to remove session

--- Resources/PHP/model/DBManager/DB_Record.php
+++ Resources/PHP/model/DBManager/DB_Record.php
@@ -380,21 +380,4 @@ class DB_Record
 
 
 
-    public function startSession() {
-
-        if (isset($_REQUEST['PHPSESSID']) || isset($_SESSION['session_id'])) {
-//            var_dump($_REQUEST, ' folgt die session', $_SESSION, 'session name', session_name());
-            if (isset($_REQUEST['PHPSESSID'])) {
-                $session_id = array($_REQUEST['PHPSESSID']);
-            } elseif (isset($_SESSION['session_id'])) {
-                $session_id = array($_SESSION['session_id']);
-            }
-            session_start($session_id);
-        } else {
-            session_start();
-            header('location: '.RP.'index.php');
-        }
-        $_SESSION['session_id'] = $_REQUEST['PHPSESSID'];
-    }
-
 }
