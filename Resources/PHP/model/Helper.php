8dc85414d346bfa098cfc29c19badaa6e3e250a9
David Goraj <d.goraj@exinit.de>
2017-06-29 14:44:21 +0200

    [task] added session to Helper.php

--- Resources/PHP/model/Helper.php
+++ Resources/PHP/model/Helper.php
@@ -8,5 +8,20 @@
  */
 class Helper
 {
+    public function startSession() {
 
+        if (isset($_REQUEST['PHPSESSID']) || isset($_SESSION['session_id'])) {
+//            var_dump($_REQUEST, ' folgt die session', $_SESSION, 'session name', session_name());
+            if (isset($_REQUEST['PHPSESSID'])) {
+                $session_id = array($_REQUEST['PHPSESSID']);
+            } elseif (isset($_SESSION['session_id'])) {
+                $session_id = array($_SESSION['session_id']);
+            }
+            session_start($session_id);
+        } else {
+            session_start();
+            header('location: '.RP.'index.php');
+        }
+        $_SESSION['session_id'] = $_REQUEST['PHPSESSID'];
+    }
 }
