c1e1973991ccb54e234b5b08c6b331c7003a1d52
Frank Berdele <f.berdel@exinit.de>
2017-06-29 09:17:44 +0200

    [task] add class AJAXController

---
+++ Resources/PHP/model/AJAXControler/AJAXController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: exinit
+ * Date: 28.06.2017
+ * Time: 17:07
+ */
+
+require_once ('../DBManager/DB_Record.php');
+
+
+class AJAXController
+{
+    private $db_record;
+
+    public function __construct()
+    {
+        $db_record = new DB_Record();
+    }
+
+    public function execute(){
+        /*
+         * Benötigte variablen für den Aufruf
+         * über JQuery
+         */
+        $methode = "";
+        $jsonObject = NULL;
+        $resultObject = "";
+        $responseObjectJSON = false;
+
+
+        //übernahme via $Session Variable möglich ??
+        if(isset($_SESSION['methode'])){
+            $methode = $_SESSION['methode'];
+        }
+
+        //übernahme via Post Variable
+        if(isset($_POST['methode'])){
+            $methode = $_POST['methode'];
+        }
+
+        if(isset($_POST['jsonObject'])){
+            $jsonObject = json_decode($_POST['jsonObject']);
+            $responseObjectJSON = true;
+        }
+
+        switch($methode){
+            case 'getMonthrecords':{
+
+                break;
+            }
+        }
+
+    }
+
+}
