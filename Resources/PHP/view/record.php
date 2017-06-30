63c04150e20224a56e322ce215c14be9385ef45c
David Goraj <d.goraj@exinit.de>
2017-06-30 11:04:08 +0200

    Merge branch 'master-berichtsheft' of https://github.com/Wollhaar/berichtsheft
    # Conflicts:
    #	Resources/PHP/model/DBManager/DB_Record.php
    #	Resources/PHP/model/DBManager/DB_User.php
    #	Resources/PHP/view/record.php
    #	Resources/Private/Layouts/main.php
    #	Resources/Private/Layouts/register.html
    #	Resources/Public/js/recordbook.js
    #	index.php
    #	recordbook.html
    
    pulled remote master-berichtsheft

--- Resources/PHP/view/record.php
+++ Resources/PHP/view/record.php
@@ -1,15 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-
-
-session_start();
-
-
-function setSessionPost(){
-
-
-=======
 require_once ('../model/DBManager/DB_Record.php');
 
 session_start();
@@ -45,7 +34,6 @@ function writeRecordDatabase(){
 
 
 function setSessionPost(){
->>>>>>> 0787f1a72d73f465f3d6899013c03cd2d63982ef
     if(!isset($_SESSION['status']))
         $_SESSION['status'] = $_POST['status'];
     if(!isset($_SESSION['ort']))
@@ -54,14 +42,6 @@ function setSessionPost(){
     if(!isset($_SESSION['record']))
         $_SESSION['record'] = $_POST['record'];
 
-<<<<<<< HEAD
-
-
-    $i=0;
-    while (isset($_POST['record' . $i])){
-        $_SESSION['record' . $i] = $_POST['record' . $i];
-        echo $_SESSION['record' . $i] . '<br>';
-=======
     $i=0;
     if(isset($_POST['record'])){
         $_SESSION[record] = $_POST['record'];
@@ -72,15 +52,10 @@ function setSessionPost(){
         $_SESSION['record' . $i] = $_POST['record' . $i];
         $_SESSION['comment' . $i] = $_POST['comment' . $i];
 
->>>>>>> 0787f1a72d73f465f3d6899013c03cd2d63982ef
         $i++;
     }
 }
 
 
-<<<<<<< HEAD
-?>
-=======
 
 
->>>>>>> 0787f1a72d73f465f3d6899013c03cd2d63982ef
