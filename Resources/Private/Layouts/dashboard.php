06af31f37adb31ce41941d1d18f0be20e845611c
David Goraj <d.goraj@exinit.de>
2017-07-03 18:18:32 +0200

    [task] edited dashboard.php to getting/presenting records

--- Resources/Private/Layouts/dashboard.php
+++ Resources/Private/Layouts/dashboard.php
@@ -21,6 +21,7 @@ require_once '../../PHP/model/config.php';
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="<?php echo RP.PUB_PATH; ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo RP.PUB_PATH; ?>js/main.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -73,12 +74,26 @@ require_once '../../PHP/model/config.php';
             <h3>Dein Dashboard</h3>
             <p>Alle relevanten Dinge sind, hier für dich notiert.</p>
             <div class="records col-md-9">
+                <?php if($_SESSION['bool'] === TRUE): ?>
+                <span>Bericht <?php echo $_SESSION['record_id']['record_id']; ?> aktualisiert.</span>
+                <?php endif; ?>
 <!--          Ausgabe der berichte und Anzeige fehlender Berichte       -->
                 <?php
+                if($_SESSION['bool'] === TRUE){echo $_SESSION['bool'];}
                 $records = new DB_Record();
                 // getting records for displaying
-                // $recordOutput = $records->
-                ?>
+                $recordOutput = $records->recordOut();
+                foreach($recordOutput as $item => $value): ?>
+                    <?php var_dump($item);?>
+                    <div class="records">
+                    <span><?php echo $value['record_id']?></span>
+                        <label class="">Bericht</label><span ><?php echo $value['recorddate']; ?></span>
+                    <div class="record"><?php echo $value['record']; ?></div>
+                        <label>Kommentar</label>
+                    <div class="comment"><?php echo $value['comment']; ?></div>
+                        <span><a href="<?php echo RP; ?>index.php?case=edit&id=<?php echo $value['record_id']; ?>" class="btn btn-link" id="record-link-<?php echo $value['record_id']; ?>">Editieren</a></span>
+                </div>
+                <?php endforeach; ?>
             </div>
             <div class="calendar col-md-3">
 <!--         ausgabe des Kalenders    -->
