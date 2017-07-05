96800009ea64d1a287d2b4fecedf90b0aa6a70ef
David Goraj <d.goraj@exinit.de>
2017-07-05 20:01:35 +0200

    [task] writing ajax script

--- Resources/Public/js/main.js
+++ Resources/Public/js/main.js
@@ -1,3 +1,14 @@
-/**
- * Created by exinit on 23.06.2017.
- */
+$(Document).ready(function () {
+
+    function loadRecords() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById('dump-record').innerHTML = this.responseText;
+            }
+        };
+
+        xhttp.open('GET', 'ajax_info.txt', TRUE);
+        xhttp.send();
+    }
+});
