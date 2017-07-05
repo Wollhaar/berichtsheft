67b88b241964c136233634825f637d3be7797741
Frank Berdele <f.berdel@exinit.de>
2017-07-05 12:19:12 +0200

    [add] constructor

--- Resources/PHP/model/AJAXControler/AJAXController.php
+++ Resources/PHP/model/AJAXControler/AJAXController.php
@@ -4,51 +4,68 @@ ini_set('display_errors', 'On');
 require_once ('../DBManager/DB_Record.php');
 
 
+class AJAXController{
 
-$dbr= new DB_Record();
-
- if(isset($_POST['method']))
-   $request = $_POST['method'];
- else
-     return '[Error] method Post Variable is not set';
+    private $dbr;
+    private $request;
 
 
-function debug_console($data){
-    if(is_array($data))
-        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
-    else
-        $output = "<script>console.log( 'Debug Objects: "  . $data . "' );</script>";
+    public function __construct(){
+        $this->dbr= new DB_Record();
 
-    echo $output;
-}
+        if(isset($_POST['method']))
+            $this->request = $_POST['method'];
+        else
+            return '[Error] method Post Variable is not set';
 
-
-switch($request){
-
-    case 'getCurrentMonth': {
-        $currentTimestamp = time();
-        $currentYear = date('Y', $currentTimestamp);
-        $currentMonth = date('m', $currentTimestamp);
-
-
-        echo $resultObject = json_encode($dbr->getRecordMonth((string)$currentYear,(string)$currentMonth));
-        break;
+        $this->controll($this->request);
     }
-    case 'getRecord': {
-        if(isset($_POST['selectRec'])){
-            $selectedDate = $_POST['selectRec'];
-            $date = explode('-', $selectedDate);
 
-            echo $resultObject = json_encode($dbr->readRecordDay($date[0], $date[1], $date[2]));
+    public function debug_console($data){
+        if(is_array($data))
+            $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+        else
+            $output = "<script>console.log( 'Debug Objects: "  . $data . "' );</script>";
 
-        }else{
-            debug_console("[Error]case: getRecord");
+        echo $output;
+    }
+
+    public function controll($request){
+        switch($request){
+
+            case 'getCurrentMonth': {
+                $currentTimestamp = time();
+                $currentYear = date('Y', $currentTimestamp);
+                $currentMonth = date('m', $currentTimestamp);
+
+
+                echo $resultObject = json_encode($this->dbr->getRecordMonth((string)$currentYear,(string)$currentMonth));
+                break;
+            }
+            case 'getRecord': {
+                if(isset($_POST['selectRec'])){
+                    $selectedDate = $_POST['selectRec'];
+                    $date = explode('-', $selectedDate);
+
+                    echo $resultObject = json_encode($this->dbr->readRecordDay($date[0], $date[1], $date[2]));
+
+                }else{
+                    debug_console("[Error]case: getRecord");
+                    return false;
+                }
+                break;
+            }
+            default: {
+                debug_console("[Error] methode Post variable: " . $request);
+                return false;
+            }
         }
-        break;
-
     }
-    default: debug_console("[Error] methode Post variable: " . $request);
 }
+
+$controller = new AJAXController();
+
+
 
 
 
