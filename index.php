ca9a8f823fedc78c9cc1642abb6063d926826f90
David Goraj <d.goraj@exinit.de>
2017-07-10 13:56:31 +0200

    [task] edit dashboard.php for beauty

--- index.php
+++ index.php
@@ -20,15 +20,14 @@ switch ($request['case']) {
     case 'login':
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $login = array( 'username' => $request['username'], 'password' => $request['password']);
+            $login = array('username' => $request['username'], 'password' => $request['password']);
             if (isset($login) && empty($user)) {
                 $user = new DB_User();
                 $check = $user->login($login['username'], $login['password']);
-            }
-            elseif (isset($login) && is_object($user)) {
+            } elseif (isset($login) && is_object($user)) {
                 $check = $user->login($login['username'], $login['password']);
             }
-            if(is_string($check)) {
+            if (is_string($check)) {
                 die($check);
             }
             if (is_object($user)) {
@@ -36,28 +35,27 @@ switch ($request['case']) {
                 if ($access === TRUE) {
                     $_SESSION['user'] = $login;
 //                    $_SESSION['case'] = 'case';
-                    header('location: '.RP.PRI_PATH.'dashboard.php');
-                }
-                elseif (is_array($access)) {
+                    header('location: ' . RP . PRI_PATH . 'dashboard.php');
+                } elseif (is_array($access)) {
                     $_SESSION['access'] = $access[0];
-                    header('location: '.RP.PRI_PATH.'login.php');
+                    header('location: ' . RP . PRI_PATH . 'login.php');
                 }
             }
         }
         break;
 
-        // register-script
+    // register-script
     case 'register':
         if (empty($_SESSION['request']) || !isset($request['ready'])) {
             $_SESSION['request'] = $request;
-            header('location: '.RP.PRI_PATH.'register.php');
+            header('location: ' . RP . PRI_PATH . 'register.php');
         }
 
         if (isset($session) && !(isset($request['username']) && isset($request['password']) && isset($request['email']))) {
             $request = $_SESSION['request'];
         }
 
-        if (isset($request['username'])&& $request['password'] && $request['email']) {
+        if (isset($request['username']) && $request['password'] && $request['email']) {
             $regist = array('username' => $request['username'], 'password' => $request['password'], 'email' => $request['email'],
                 'first_name' => $request['first_name'], 'last_name' => $request['last_name'], 'adress' => $request['adress'],
                 'PLZ' => $request['PLZ'], 'place' => $request['place'], 'birthday' => $request['birthday']);
@@ -69,14 +67,12 @@ switch ($request['case']) {
             }
             if ($fb['check'] === true) {
                 $_SESSION['user'] = $fb;
-                header('location: '.RP.PRI_PATH.'dashboard.php');
+                header('location: ' . RP . PRI_PATH . 'dashboard.php');
                 break;
-            }
-            else {
+            } else {
                 die('Sorry, something went wrong. Please try again later. If you have time, give us feedback on david@exinit.de');
             }
-        }
-        else {
+        } else {
             echo 'please set all fields';
         }
 
@@ -88,7 +84,18 @@ switch ($request['case']) {
             header('location: ' . RP . PRI_PATH . 'login.php');
             break;
         }
-        header('location: '.RP.PRI_PATH.'account.php');
+        header('location: ' . RP . PRI_PATH . 'account.php');
+        break;
+
+
+    case 'getter':
+        if (empty($records)) {
+            $records = new DB_Record();
+            $records->recordOut($_SESSION['user']['username'], $request['load']);
+        }
+        else {
+            $records->recordOut($_SESSION['user']['username'], $request['load']);
+        }
         break;
 
     case 'record':
