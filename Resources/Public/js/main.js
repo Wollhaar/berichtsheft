a7fd384cc98f7ce604e7bd763099f31b10f85bb7
David Goraj <d.goraj@exinit.de>
2017-07-11 18:55:26 +0200

    [task] edit main.js to get over AJAX records for dashboard(but not ready yet)

--- Resources/Public/js/main.js
+++ Resources/Public/js/main.js
@@ -1,6 +1,6 @@
 $(Document).ready(function () {
 
-    function loadRecords() {
+    document.getElementById('btn-forward').onclick(function forwardLoadRecords(load) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
@@ -8,7 +8,21 @@ $(Document).ready(function () {
             }
         };
 
-        xhttp.open('GET', 'ajax_info.txt', TRUE);
-        xhttp.send();
-    }
+        console.log(load);
+        xhttp.open('POST', 'index.php?case=getter', TRUE);
+        xhttp.send('load='+load);
+    });
+
+    document.getElementById('btn-back').onclick(function backLoadRecords(load) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById('dump-record').innerHTML = this.responseText;
+            }
+        };
+
+        console.log(load);
+        xhttp.open('POST', 'index.php?case=getter', TRUE);
+        xhttp.send('load='+load);
+    });
 });
