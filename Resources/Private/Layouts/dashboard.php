535d8326d275fc6c94c875404149feef1614b4f3
David Goraj <d.goraj@exinit.de>
2017-07-11 18:53:15 +0200

    [task] edit dashboard.php for routing and getting 5 records every reload

--- Resources/Private/Layouts/dashboard.php
+++ Resources/Private/Layouts/dashboard.php
@@ -15,13 +15,12 @@ require_once '../../PHP/model/config.php';
     <title>Berichtsheft-Tool</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo RP.PUB_PATH; ?>css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo RP.PUB_PATH; ?>css/bootstrap-theme.css" rel="stylesheet">
-    <link href="<?php echo RP.PUB_PATH; ?>css/main.css" rel="stylesheet">
+    <link href="<?php echo PUB_PATH; ?>css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo PUB_PATH; ?>css/bootstrap-theme.css" rel="stylesheet">
+    <link href="<?php echo PUB_PATH; ?>css/main.css" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="<?php echo RP.PUB_PATH; ?>js/bootstrap.min.js"></script>
-    <script src="<?php echo RP.PUB_PATH; ?>js/main.js"></script>
+    <script src="<?php echo PUB_PATH; ?>js/bootstrap.min.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -68,42 +67,45 @@ require_once '../../PHP/model/config.php';
     <div class="board">
 
         <?php if($_SESSION['user']['check'] === true): ?>
-            <?php include_once RP.PRI_PATH.'Partials/welcome.html'; ?>
+            <?php include_once PRI_PATH.'Partials/welcome.html'; ?>
         <?php endif; ?>
-<!--        --><?php //var_dump($_SESSION); ?>
+        <!--        --><?php //var_dump($_SESSION); ?>
 
         <div class="dashboard">
             <h3>Dein Dashboard</h3>
             <p>Alle relevanten Dinge sind, hier für dich notiert.</p>
 
-            <div class="records col-md-9">
-                <?php if($_SESSION['bool'] === TRUE): ?>
-                <span>Bericht <?php echo $_SESSION['record_id']['record_id']; ?> aktualisiert.</span>
-                <?php endif; ?>
+            <div class="col-md-9">
+                <div class="records" id="dump-record">
+                    <?php if(isset($_SESSION['bool']) && $_SESSION['bool'] === TRUE): ?>
+                        <span>Bericht <?php echo $_SESSION['record_id']['record_id']; ?> aktualisiert.</span>
+                    <?php endif; ?>
 
-<!--          Ausgabe der berichte und Anzeige fehlender Berichte       -->
-                <?php
-                if($_SESSION['bool'] === TRUE){echo $_SESSION['bool'];}
-                $records = new DB_Record();
+                    <!--          Ausgabe der berichte und Anzeige fehlender Berichte       -->
+                    <?php
+                    // if(isset($_SESSION['bool']) && $_SESSION['bool'] === TRUE){echo $_SESSION['bool'];}
+                    $records = new DB_Record();
 
-                // getting records for displaying
-                $recordOutput = $records->recordOut($_SESSION['user']['username']);
+                    // getting records for displaying
+                    $recordOutput = array_reverse($records->recordOut($_SESSION['user']['username']), TRUE);
 
 //                begin of output
                 foreach($recordOutput as $item => $value): ?>
-<!--                    --><?php //var_dump($value);?>
+                    <!--                    --><?php //var_dump($value);?>
                     <div class="records">
-                    <span><?php echo $value['record_id']; ?></span>
+                        <span><?php echo $value['record_id']; ?></span>
                         <label class="">Bericht</label><span ><?php echo $value['recorddate']; ?></span>
-                    <div class="record"><?php echo $value['records']; ?></div>
+                        <div class="record"><?php echo $value['records']; ?></div>
                         <label>Kommentar</label>
-                    <div class="comment"><?php echo $value['comment']; ?></div>
+                        <div class="comment"><?php echo $value['comment']; ?></div>
                         <span><a href="<?php echo RP; ?>index.php?case=edit&id=<?php echo $value['record_id']; ?>" class="btn btn-link" id="record-link-<?php echo $value['record_id']; ?>">Editieren</a></span>
-                </div>
+                    </div>
                 <?php endforeach; ?>
-                <hr/>
-                <button onclick="loadRecords()">Zurück</button><button onclick="loadRecords()">Weiter</button>
-<!--        add new records        -->
+                    <hr/>
+                    <?php if($_SESSION['counter'] == $_SESSION['rows']): ?><button id="btn-back" onclick="backLoadRecords('back');">Zurück</button><?php endif;
+                if($_SESSION['counter'] > 6): ?><button id="btn-forward" onclick="forwardLoadRecords('forward');">Weiter</button><?php endif; ?>
+                </div>
+                <!--        add new records        -->
                 <div class="add-record container">
                     <!-- form sendet zum speichern und hinzufügen der Berichte, die Daten            -->
                     <h3>Neuen Bericht hinzufügen:</h3>
@@ -119,9 +121,9 @@ require_once '../../PHP/model/config.php';
                 </div>
             </div>
             <div class="calendar col-md-3">
-<!--         ausgabe des Kalenders    -->
+                <!--         ausgabe des Kalenders    -->
                 <ul class="calendar-list">
-                <?php $days = $records->getRecordMonth(2017, 6);
+                    <?php $days = $records->getRecordMonth(2017, 6);
                 foreach ($days as $day) { ?>
                     <li><?php echo $day; ?> </li>
                 <?php } ?>
@@ -130,5 +132,8 @@ require_once '../../PHP/model/config.php';
         </div>
     </div>
 </div>
+<footer>
+    <script src="<?php echo PUB_PATH; ?>js/main.js"></script>
+</footer>
 </body>
 </html>
