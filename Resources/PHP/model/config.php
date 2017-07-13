b0dec31ff44f73692c218696afefeba758b3b892
David Goraj <d.goraj@exinit.de>
2017-07-13 18:07:51 +0200

    [task] edit config.php for routing

--- Resources/PHP/model/config.php
+++ Resources/PHP/model/config.php
@@ -7,7 +7,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('RP', substr($_SERVER['DOCUMENT_ROOT'] , 29, 30).DS); // eventuelle Änderung, bei Wechsel auf anderen Server.
 define('PUB_PATH', RP.'Resources'.DS.'Public'.DS); // DIRECTORY_SEPERATOR evtll gegen '/' austauschen
 define('PRI_PATH', RP.'Resources'.DS.'Private'.DS.'Layouts'.DS);
-define('MOD_PATH', RP.'Resources'.DS.'PHP'.DS.'Model'.DS);
+define('MOD_PATH', RP.'Resources'.DS.'PHP'.DS.'model'.DS);
 
 $session = new Helper();
 $session->startSession();
@@ -15,11 +15,12 @@ $session->startSession();
 /*include_once 'DBManager/DB_Connection.php';
 include_once 'DBManager/DB_User.php';
 include_once 'Helper.php';*/
+//echo MOD_PATH;
 
 function __autoload ($classname) {
 //    echo $classname.'<br/>';
     if (substr($classname, 0, 2) == 'DB') {
-        require 'DBManager/'.$classname.'.php';
+        require 'DBManager'.DS.$classname.'.php';
     }
     elseif ($classname == 'Helper') {
         require_once 'Helper.php';
