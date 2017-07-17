931fc7367282932414efe8d6ac6f3b06c90d8867
David Goraj <d.goraj@exinit.de>
2017-07-17 18:55:34 +0200

    [task] edit Helper.php to fix session | not done yet

--- Resources/PHP/model/Helper.php
+++ Resources/PHP/model/Helper.php
@@ -8,18 +8,34 @@
  */
 class Helper
 {
-    public function startSession($user = NULL, $restart = NULL)
-    {
+    private $user;
+    private $session;
 
-        if (isset($restart) && !empty($user['session_id'])) {
+
+    public function __construct()
+    {
+//        $this->user = $_SESSION['user'];
+        if (isset($_REQUEST)) {
+            $this->session = $_REQUEST['key'];
+//            $this->session = $_REQUEST[$_REQUEST['key']];
+        }
+    }
+
+    public function startSession($user = NULL, $session_id = NULL)
+    {
+//        var_dump($this->user, $_REQUEST, session_name(), session_id());
+
+        if (!isset($session_id) && !empty($this->session)) {
 //            var_dump($_REQUEST, ' folgt die session', $_SESSION, 'session name', session_name());
-            session_name($user['session_id']);
+            session_name($this->getSessionName());
         }
 
 //      die session soll erst gestartet werden, wenn sich ein user angemeldet hat.
         elseif (isset($user)) {
             session_name($user['username'].'_'.$user['user_id']);
             session_start();
+
+//            $this->sendPost($user['username'].'_'.$user['user_id']);
 //            header('location: '.RP.'index.php');
         }
 
@@ -28,6 +44,35 @@ class Helper
             session_start($session_id);
         }
 
-        $_SESSION['session_id'] = $_REQUEST['PHPSESSID'];
+        if (isset($_REQUEST['PHPSESSID'])) {
+            $_SESSION['session_id'] = $_REQUEST['PHPSESSID'];
+        }
+    }
+
+    public function getUserSession() {
+        return $this->user;
+    }
+
+    public function sendPost($value) {
+        $url = 'https://report-tool.david.exinitdev.de/index.php';
+        $data = array('key' => $value);
+
+// use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { echo 'POST failed!'; }
+
+        var_dump($result);
+    }
+
+    public function getSessionName() {
+        return $this->session;
     }
 }
