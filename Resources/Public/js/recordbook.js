c1472784e0e33f403032f624abf830887f9b890c
Frank Berdele <f.berdel@exinit.de>
2017-07-19 09:41:06 +0200

    [fix] code formation

--- Resources/Public/js/recordbook.js
+++ Resources/Public/js/recordbook.js
@@ -1,13 +1,3 @@
-/**
- * Created by exinit on 19.06.2017.
- */
-
-
-
-
-
-
-
 function loadCurrentMonth()
 {
   //url	Required. Specifies the URL you wish to load
@@ -223,74 +213,64 @@ function setSelectField(place, status)
 
 var recordCount = 0;
 
+function addRecord()
+{
 
-
-
-
-
-function addRecord(){
-
-  if(recordCount == 0){
+  if (recordCount == 0) {
     document.getElementById('end').innerHTML =
-      '<div class="row" id="newRecordDiv'+ recordCount + '">' +
-        '<div class="row">' +
-          '<div class="col-md-1 col-md-offset-1 form-group">' +
-            '<label class="sr-only"></label>' +
-            '<button id="delRecordButton' + recordCount +'" class="form-control" value="-" type="button" onclick="delRecord(this.id);">-</button>' +
-          '</div>'+
-          '<div class="col-md-3 form-group">' +
-            '<label class="sr-only"></label>' +
-            '<input class="form-control" id="record' + recordCount +'" name="record' + recordCount + '" type="text" value="Berichtshefteintrag">' +
-          '</div> ' +
-          '<div class="col-md-1 form-group"> ' +
-            '<label class="sr-only"></label>' +
-            '<input class="form-control" id="time' + recordCount +'" name="time' + recordCount + '" type="text" value="00:h 00:m">' +
-          '</div> ' +
-          '<div class="col-md-4 form-group">' +
-            '<label class="sr-only"></label>' +
-            '<input class="form-control" id="comment' + recordCount + '" name="comment' + recordCount + '" type="text" value="Kommentare zum Eintrag">' +
-          '</div>' +
-        '</div>' +
+      '<div class="row" id="newRecordDiv' + recordCount + '">' +
+      '<div class="row">' +
+      '<div class="col-md-1 col-md-offset-1 form-group">' +
+      '<label class="sr-only"></label>' +
+      '<button id="delRecordButton' + recordCount + '" class="form-control" value="-" type="button" onclick="delRecord(this.id);">-</button>' +
       '</div>' +
-      '<div id="end' + recordCount +'"></div>';
-
-
-
-
-
+      '<div class="col-md-3 form-group">' +
+      '<label class="sr-only"></label>' +
+      '<input class="form-control" id="record' + recordCount + '" name="record' + recordCount + '" type="text" value="Berichtshefteintrag">' +
+      '</div> ' +
+      '<div class="col-md-1 form-group"> ' +
+      '<label class="sr-only"></label>' +
+      '<input class="form-control" id="time' + recordCount + '" name="time' + recordCount + '" type="text" value="00:h 00:m">' +
+      '</div> ' +
+      '<div class="col-md-4 form-group">' +
+      '<label class="sr-only"></label>' +
+      '<input class="form-control" id="comment' + recordCount + '" name="comment' + recordCount + '" type="text" value="Kommentare zum Eintrag">' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+      '<div id="end' + recordCount + '"></div>';
 
   }
-  else{
-    document.getElementById('end'+ (recordCount-1)).innerHTML =
-      '<div class="row" id="newRecordDiv'+ recordCount + '">' +
-        '<div class="row">' +
-          '<div class="col-md-1 col-md-offset-1">' +
-            '<button id="delRecordButton' + recordCount +'" class="form-control" value="delRecordButton' + recordCount + '" type="button" onclick="delRecord(this.id);">-</button>' +
-          '</div>' +
+  else {
+    document.getElementById('end' + (recordCount - 1)).innerHTML =
+      '<div class="row" id="newRecordDiv' + recordCount + '">' +
+      '<div class="row">' +
+      '<div class="col-md-1 col-md-offset-1">' +
+      '<button id="delRecordButton' + recordCount + '" class="form-control" value="delRecordButton' + recordCount + '" type="button" onclick="delRecord(this.id);">-</button>' +
+      '</div>' +
       '<div class="col-md-3">' +
-      '<input class="form-control" id="record' + recordCount +'" name="record' + recordCount + '" type="text" value="Berichtshefteintrag">' +
+      '<input class="form-control" id="record' + recordCount + '" name="record' + recordCount + '" type="text" value="Berichtshefteintrag">' +
       '</div> ' +
       '<div class="col-md-1"> ' +
-      '<input class="form-control" id="time' + recordCount +'" name="time' + recordCount + '" type="text" value="00:h 00:m">' +
+      '<input class="form-control" id="time' + recordCount + '" name="time' + recordCount + '" type="text" value="00:h 00:m">' +
       '</div> ' +
       '<div class="col-md-4">' +
       '<input class="form-control" id="comment' + recordCount + '" name="comment' + recordCount + '" type="text" value="Kommentare zum Eintrag">' +
       '</div>' +
       '</div>' +
       '</div>' +
-      '<div id="end' + recordCount +'"></div>';
+      '<div id="end' + recordCount + '"></div>';
   }
 
   return recordCount++;
 }
 
-
-function delRecord(clicked_id){
+function delRecord(clicked_id)
+{
   var id = document.getElementById(clicked_id).id;
   id = id.slice(15, 16);
-    var record = document.getElementById('newRecordDiv' + id);
-    record.parentNode.removeChild(record);
+  var record = document.getElementById('newRecordDiv' + id);
+  record.parentNode.removeChild(record);
 
-
-    return recordCount--;
+  return recordCount--;
 }
