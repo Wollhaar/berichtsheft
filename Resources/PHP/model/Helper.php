64c19fdba76035ed86b6d68f4316a74b5c047efa
David Goraj <d.goraj@exinit.de>
2017-07-21 14:56:13 +0200

    [task] edit Helper.php to work on session and seperating users

--- Resources/PHP/model/Helper.php
+++ Resources/PHP/model/Helper.php
@@ -15,7 +15,7 @@ class Helper
     public function __construct()
     {
 //        $this->user = $_SESSION['user'];
-        if (isset($_REQUEST)) {
+        if (isset($_REQUEST['key'])) {
             $this->session = $_REQUEST['key'];
 //            $this->session = $_REQUEST[$_REQUEST['key']];
         }
@@ -25,23 +25,25 @@ class Helper
     {
 //        var_dump($this->user, $_REQUEST, session_name(), session_id());
 
-        if (!isset($session_id) && !empty($this->session)) {
+        if (!isset($session_id) && !empty($session_id['key'])) {
 //            var_dump($_REQUEST, ' folgt die session', $_SESSION, 'session name', session_name());
-            session_name($this->getSessionName());
+            session_name($session_id['key']);
         }
 
 //      die session soll erst gestartet werden, wenn sich ein user angemeldet hat.
         elseif (isset($user)) {
-            session_name($user['username'].'_'.$user['user_id']);
+//            session_name($user['username'].'_'.$user['user_id']);
             session_start();
+            $_SESSION['session_id'] = session_id();
 
+//            $this->sendPost(session_name());
 //            $this->sendPost($user['username'].'_'.$user['user_id']);
 //            header('location: '.RP.'index.php');
         }
 
         //  restart einer laufenden session
         if(isset($session_id)) {
-            session_start($session_id);
+            session_start(array($session_id));
         }
 
         if (isset($_REQUEST['PHPSESSID'])) {
@@ -49,30 +51,34 @@ class Helper
         }
     }
 
-    public function getUserSession() {
-        return $this->user;
-    }
 
+    // ------------------------------------------  temporally not in use ------------------------------------------
+//https://konsoleh.your-server.de/?domain=report-tool.david.exinitdev.de
     public function sendPost($value) {
-        $url = 'https://report-tool.david.exinitdev.de/index.php';
+        $url = $_SERVER['DOCUMENT_ROOT'].'/index.php';
         $data = array('key' => $value);
 
 // use key 'http' even if you send the request to https://...
         $options = array(
             'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n"
+                    . "Content-Length: " . strlen($data['key']) . "\r\n",
                 'method'  => 'POST',
-                'content' => http_build_query($data)
+                'content' => $data['key']
             )
         );
         $context  = stream_context_create($options);
         $result = file_get_contents($url, false, $context);
         if ($result === FALSE) { echo 'POST failed!'; }
-
+/*echo '<pre>';
         var_dump($result);
+        echo '</pre>';*/
     }
-
     public function getSessionName() {
         return $this->session;
+    }
+
+    public function getUserSession() {
+        return $this->user;
     }
 }
