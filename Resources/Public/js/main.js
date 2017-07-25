03d1f48c6843c0226b6143446882b55e789f97ac
David Goraj <d.goraj@exinit.de>
2017-07-25 18:11:48 +0200

    [task] edit main.js for ajax

--- Resources/Public/js/main.js
+++ Resources/Public/js/main.js
@@ -1,6 +1,30 @@
-$(Document).ready(function () {
 
-    document.getElementById('btn-forward').onclick(function forwardLoadRecords(load) {
+
+
+    function forwardLoadRecords(load) {
+
+            $("button").click(function(){
+                $.ajax({url: "/index.php?case=getter&load="+load, success: function(result){
+                    $("#dump-record").html(result);
+                }});
+            });
+
+    /*var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById('dump-record').innerHTML = this.responseText;
+            }
+            else {
+                console.log(xhttp.responseText);
+            }
+        };
+
+        xhttp.open('POST', '/index.php?case=getter'/!*, TRUE*!/);
+        xhttp.send("load=" + load);*/
+        // console.log(xhttp);
+    }
+
+    function backLoadRecords(load) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
@@ -9,38 +33,7 @@ $(Document).ready(function () {
         };
 
         console.log(load);
-        xhttp.open('POST', 'index.php?case=getter', TRUE);
+        xhttp.open('POST', 'index.php?case=getter');
         xhttp.send('load=' + load);
-    });
+    }
 
-    document.getElementById('btn-back').onclick(function backLoadRecords(load) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById('dump-record').innerHTML = this.responseText;
-            }
-        };
-
-        console.log(load);
-        xhttp.open('POST', 'index.php?case=getter', TRUE);
-        xhttp.send('load=' + load);
-    });
-
-
-        document.getElementById('profile').onclick(function () {
-
-            $('.profile-box').toggle();
-        });
-
-});
-
-// javascript
-function getStyleElement(id) {
-    var style;
-    style = document.defaultView.getComputedStyle(document.getElementById(id), '').getPropertyValue("display");
-    return style;
-}
-
-function toggle_box() {
-
-}
