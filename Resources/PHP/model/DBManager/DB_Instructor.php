b7fde6f0102d32a0e9f544eabdb66daa83733d1d
Frank Berdel <f.berdel@exinit.de>
2017-08-01 17:17:30 +0200

    [add] function writeInstructor -> writes instructor information into DB_Instructor

--- Resources/PHP/model/DBManager/DB_Instructor.php
+++ Resources/PHP/model/DBManager/DB_Instructor.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'On');
 
-
+include_once ($_SERVER['DOCUMENT_ROOT'] . '/Resources/PHP/model/DBManager/DB_Connection.php');
 
 
 class DB_Instructor extends DB_Connection
@@ -13,7 +13,7 @@ class DB_Instructor extends DB_Connection
 
         try {
             $this->dbc->beginTransaction();
-            $sth = $this->dbc->prepare('SELECT name, vorname, role, location, imgPath FROM recordbook.instructor');
+            $sth = $this->dbc->prepare('SELECT name, vorname, role, location, imgPath, content FROM recordbook.instructor');
 
             $sth->execute();
             $record = $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -31,5 +31,31 @@ class DB_Instructor extends DB_Connection
         }
 
         return true;
+    }
+
+    public function writeInstructor($name, $vorname, $location, $role, $content){
+        $this->getConnection();
+
+        try{
+            $this->dbc->beginTransaction();
+            $sth = $this->dbc->prepare("INSERT INTO instructor (name, vorname, location, role, content) VALUES (?, ?, ?, ?, ?)");
+
+            $sth->bindParam(1, $name, PDO::PARAM_STR);
+            $sth->bindParam(2, $vorname, PDO::PARAM_STR);
+            $sth->bindParam(3, $location, PDO::PARAM_STR);
+            $sth->bindParam(4, $role, PDO::PARAM_STR);
+            $sth->bindParam(5, $content, PDO::PARAM_STR);
+
+            $sth->execute();
+            $this->dbc->commit();
+
+            return true;
+
+
+        }catch(PDOException $exception){
+            $this->dbc->rollBack();
+            echo('[Error ] ' . $exception->getMessage() . 'in Line: ' . $exception->getLine());
+            return false;
+        }
     }
 }
