a286de54b93ac5062fb7b5738f1a5fdb38f4913d
David Goraj <d.goraj@exinit.de>
2017-08-02 10:07:12 +0200

    [task] edit main.js for to change between textarea and paragraph

--- Resources/Public/js/main.js
+++ Resources/Public/js/main.js
@@ -1,3 +1,43 @@
+$('div.record-box div > p').on('click', function () {
+    var text = $('div > p').text();
+    $(this).parent('div').html(function () {
+        $(this).append('<textarea class="text-edit" name="record">'+text+'</textarea>');
+    });
+    $(this).remove();
+
+    $('div.record-box div > textarea.text-edit').on('focus', function () {
+        $(document).mouseup(function(e) {
+
+            var container = $('textarea.text-edit');
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+
+                var text = $('div > textarea.text-edit').text();
+                $(this).parent('div').html(function () {
+                    $(this).append('<p>' + text + '</p>');
+                });
+                $(this).remove();
+
+                // transmitting content over ajax and save in DB
+                $.ajax({url: "/index.php?case=save&record=" + text, success: function (result) {console.log(result);} });
+            }
+        });
+
+        $('textarea.text-edit').keypress(function (e) {
+
+            var key = e.which;
+            if (key == 13) {
+                var text = $('div > textarea.text-edit').text();
+                $(this).parent('div').html(function () {
+                    $(this).append('<p>' + text + '</p>');
+                });
+                $(this).remove();
+
+                // transmitting content over ajax and save in DB
+                $.ajax({url: "/index.php?case=save&record=" + text, success: function (result) {console.log(result);} });
+            }
+        });
+    });
+});
 
 
 
