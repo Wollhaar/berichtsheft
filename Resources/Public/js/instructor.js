a6f4fb1a9a5eb821b6da5fa7404c88dcfe54ef1c
Frank Berdel <f.berdel@exinit.de>
2017-08-03 17:12:17 +0200

    [fix] in function addInstructor -> var instructForm -> severell changes on id's [add] functions 4 Drag and Drop [add] function updateInstructor [add] function transmit

--- Resources/Public/js/instructor.js
+++ Resources/Public/js/instructor.js
@@ -4,45 +4,41 @@ aus der Session variable Instructor dementsprechende Instructoren ziehen
 anzahl benötigter Felder erzeugen und mit Inhalt füllen
  */
 
-
-
 function addInstructor()
 {
-
-  console.log("Funktion durchgelaufen");
 
   var instructorForm = "<form id='instructorForm' method='post'>\n" +
     "      <div class=\"row\">\n" +
     "        <div class=\"col-md-4\">\n" +
     "          <div class=\"form-group\">\n" +
     "            <label for=\"instructorName\">Name</label>\n" +
-    "            <input type=\"text\" class=\"form-control\" id=\"instructorName\"></input>\n" +
+    "            <input type=\"text\" class=\"form-control formInput\" id=\"instructorName\"></input>\n" +
     "          </div>\n" +
     "          <div class=\"form-group\">\n" +
     "            <label for=\"instructorVorname\">Vorname</label>\n" +
-    "            <input type=\"text\" class=\"form-control\" id=\"instructorVorname\"></input>\n" +
+    "            <input type=\"text\" class=\"form-control formInput\" id=\"instructorVorname\"></input>\n" +
     "          </div>\n" +
     "          <div class=\"form-group\">\n" +
     "            <label for=\"instructorRolle\">Rolle</label>\n" +
-    "            <input type=\"text\" class=\"form-control\" id=\"instructorRolle\"></input>\n" +
+    "            <input type=\"text\" class=\"form-control formInput\" id=\"instructorRolle\"></input>\n" +
     "          </div>\n" +
     "        </div>\n" +
     "        <div class=\"col-md-4\">\n" +
     "          <div class=\"form-group\">\n" +
     "            <label for=\"location\">Location</label>\n" +
-    "            <select class=\"form-control\" id=\"location\">\n" +
-    "              <option value=\"Betrieb\">Betrieb</option>\n" +
-    "              <option value=\"Schule\">Schule</option>\n" +
-    "              <option value=\"Extern\">Extern</option>\n" +
+    "            <select class=\"form-control formInput\" id=\"location\">\n" +
+    "              <option value=\"Betrieb\" class=\"location\">Betrieb</option>\n" +
+    "              <option value=\"Schule\" class=\"location\">Schule</option>\n" +
+    "              <option value=\"Extern\" class=\"location\">Extern</option>\n" +
     "            </select>\n" +
     "          </div>\n" +
     "          <div class=\"form-group\">\n" +
     "            <label for=\"content\">Kurzbeschreibung</label>\n" +
-    "            <textarea class=\"form-control\" id=\"content\" rows=\"6\"></textarea>\n" +
+    "            <textarea class=\"form-control formInput\" id=\"content\" rows=\"6\"></textarea>\n" +
     "          </div>\n" +
     "          <div class=\"form-group\">\n" +
-    "            <button type=\"submit\" class=\"btn btn-default\"  onclick=\"addDB();\">\n" +
-    "              <span class=\"glyphicon glyphicon glyphicon-ok\" aria-hidden=\"true\"></span>\n" +
+    "            <button type=\"button\" id=\"submitB\" class=\"btn btn-default formInput\"  onclick=\"addDB();\">\n" +
+    "              <span class=\"glyphicon glyphicon glyphicon-ok formInput\" aria-hidden=\"true\"></span>\n" +
     "            </button>\n" +
     "            <button type=\"button\" class=\"btn btn-default\" onclick=\"reseter();\">\n" +
     "              <span class=\"glyphicon glyphicon glyphicon-remove\" aria-hidden=\"true\" ></span>\n" +
@@ -52,26 +48,16 @@ function addInstructor()
     "      </div>\n" +
     "    </form>"
 
-  document.getElementById("jumb").innerHTML = instructorForm;
+  document.getElementById("edit").innerHTML = instructorForm;
 
 }
 
 function reseter()
 {
   console.log("Do something");
-  var parent = document.getElementById("jumb");
+  var parent = document.getElementById('edit');
   var chield = document.getElementById('instructorForm');
   parent.removeChild(chield);
-
-
-  var addForm = "<div class=\"row\">\n" +
-    "      <div class=\"col-md-2\">\n" +
-    "        <button type=\"button\" class=\"btn btn-default\" id=\"addInstructor\" onclick=\"addInstructor()\">\n" +
-    "          <span class=\"glyphicon glyphicon glyphicon-plus\" aria-hidden=\"true\" ></span>\n" +
-    "          <label for=\"addInstructor\">Instructor hinzufügen</label>\n" +
-    "        </button>\n" +
-    "      </div>\n" +
-    "    </div>";
 
   document.getElementById("jumb").innerHTML = addForm;
 
@@ -88,7 +74,8 @@ function addDB()
     vorname: document.getElementById('instructorVorname').value,
     rolle: document.getElementById('instructorRolle').value,
     location: document.getElementById('location').value,
-    content: document.getElementById('content').value};
+    content: document.getElementById('content').value
+  };
   var dataType = "text";
 
   $.post(URL, data, function (response, status, xhr) {
@@ -102,8 +89,96 @@ function addDB()
 
 }
 
+function allowDrop(ev)
+{
+  ev.preventDefault();
+}
+
+function drag(ev)
+{
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev)
+{
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function updateInstructor(inputElement)
+{
+
+  var url = "http://recordbook.frankb.exinitdev.de/Resources/PHP/model/AJAXControler/AJAXController.php";
+  var data = {
+    method: 'getSingleInstructor',
+    single_id: document.getElementById('instructorId').value
+  }
+  var dataType = "text";
+  $.post(url, data, function (response, status, xhr) {
+    if (status != 'success') {
+      console.log("Status: " + status + " XHR: " + xhr);
+    }
+
+    console.log(response);
+    var jsonObject = JSON.parse(response);
+    console.log(jsonObject[0]);
 
 
+  }), dataType
+
+
+  var string = inputElement.value;
+  var splitter = string.split("\t");
+
+  console.log(inputElement.parentNode.getAttributeNode('id'));
+
+  var test = document.getElementById('jumbDrop');
+  if (inputElement.parentNode.getAttributeNode('id') == test.getAttributeNode('id')) {
+
+    addInstructor();
+
+    document.getElementById('instructorName').value = splitter[0];
+    document.getElementById('instructorVorname').value = splitter[1];
+    document.getElementById('instructorRolle').value = splitter[2];
+    document.getElementById('location').value = document.getElementById('location') ;
+    document.getElementById('content').value = document.getElementById('content');
+//    document.getElementById('imgPath').value = $() ;
+
+    document.getElementById('submitB').setAttribute("onclick", "transmit()");
+  }
+
+}
+
+function transmit()
+{
+  var url = "http://recordbook.frankb.exinitdev.de/Resources/PHP/model/AJAXControler/AJAXController.php";
+  var data = {
+    method: "updateInstructor",
+    name: document.getElementById('instructorName').value,
+    vorname: document.getElementById('instructorVorname').value,
+    role: document.getElementById('instructorRolle').value,
+    location: document.getElementById('location').value,
+    content: document.getElementById('content').value,
+    instructor_id: document.getElementById('instructorId').value
+  };
+
+  var dataType = "text";
+
+  $.post(url, data, function (response, status, xhr) {
+    if (status != 'success') {
+      console.log("Status: " + status + " XHR: " + xhr);
+    }
+
+    var jsonObject = JSON.parse(response);
+    if (jsonObject == true) {
+      reseter();
+      console.log(jsonObject);
+    }
+
+  }), dataType;
+
+}
 
 function showIstructors(ca)
 {
@@ -138,13 +213,14 @@ function showIstructors(ca)
           "        <label><h2>Betrieblicher Ausbilder</h2></label>";
 
         var form =
-              "        <form class=\"form-horizontal\">" +
+              "        <form class=\"form-horizontal\" >" +
               "          <div class=\"form-group\">" +
               "            <div class=\"col-md-6\">" +
-              "              <input type=\"text\" class=\"form-control inputInstructors\" value='" + jsonObject[i]['name'] + jsonObject[i]['vorname'] + jsonObject[i]['role'] + "' readonly=\"true\">" +
+              "              <input id='drag" + i + "' draggable=\"true\" ondragstart=\"drag(event)\" type=\"text\" class=\"form-control inputInstructors\" value='" + jsonObject[i]['name'] + "\t" + jsonObject[i]['vorname'] + "\t" + jsonObject[i]['role'] + "' readonly=\"true\" onclick=\"updateInstructor(this)\">" +
               "            </div>" +
               "            <a href=\"#\" data-toggle=\"popover" + i + "\" data-trigger=\"focus\"><img src=\"img/Icon/Informations-Icon.png\"></a>" +
               "          </div>" +
+              "           <input id='instructorId' value='" + jsonObject[i]['instructor_id'] + "'></input>" +
               "        </form>";
 
         /*
@@ -191,10 +267,11 @@ function showIstructors(ca)
               "        <form class=\"form-horizontal\">" +
               "          <div class=\"form-group\">" +
               "            <div class=\"col-md-6\">" +
-              "               <input type=\"text\" class=\"form-control inputInstructors\" value='" + jsonObject[i]['name'] + jsonObject[i]['vorname'] + jsonObject[i]['role'] + "' readonly=\"true\">" +
+              "              <input id='drag" + i + "' draggable=\"true\" ondragstart=\"drag(event)\" type=\"text\" class=\"form-control inputInstructors\" value='" + jsonObject[i]['name'] + "\t" + jsonObject[i]['vorname'] + "\t" + jsonObject[i]['role'] + "' readonly=\"true\" onclick=\"updateInstructor(this)\">" +
               "            </div>" +
               "            <a href=\"#\" data-toggle=\"popover" + i + "\" data-trigger=\"focus\"><img src=\"img/Icon/Informations-Icon.png\"></a>" +
               "          </div>" +
+              "           <input style='visibility: hidden' id='instructorId' value='" + jsonObject[i]['instructor_id'] + "'></input>" +
               "        </form>";
 
         var popoverScript = "<script>" +
@@ -237,10 +314,11 @@ function showIstructors(ca)
               "        <form class=\"form-horizontal\">" +
               "          <div class=\"form-group\">" +
               "            <div class=\"col-md-6\">" +
-              "               <input type=\"text\" class=\"form-control inputInstructors\" value='" + jsonObject[i]['name'] + jsonObject[i]['vorname'] + jsonObject[i]['role'] + "' readonly=\"true\">" +
+              "              <input id='drag" + i + "' draggable=\"true\" ondragstart=\"drag(event)\" type=\"text\" class=\"form-control inputInstructors\" value='" + jsonObject[i]['name'] + "\t" + jsonObject[i]['vorname'] + "\t" + jsonObject[i]['role'] + "' readonly=\"true\" onclick=\"updateInstructor(this)\">" +
               "            </div>" +
               "            <a href=\"#\" data-toggle=\"popover" + i + "\" data-trigger=\"focus\"><img src=\"img/Icon/Informations-Icon.png\"></a>" +
               "          </div>" +
+              "           <input style='visibility: hidden' id='instructorId' value='" + jsonObject[i]['instructor_id'] + "'></input>" +
               "        </form>";
 
         var popoverScript = "<script>" +
