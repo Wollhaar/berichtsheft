40e22180a96978ff0efe5011f6d6a059e3ed9c65
Frank Berdel <f.berdel@exinit.de>
2017-08-03 17:05:05 +0200

    [add] function getSingleInstructor -> returns 1 spezifik instructor with his id [add] function updateInstructor -> change Information of 1 spezifik instructor

--- Resources/PHP/model/DBManager/DB_Instructor.php
+++ Resources/PHP/model/DBManager/DB_Instructor.php
@@ -2,18 +2,18 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'On');
 
-include_once ($_SERVER['DOCUMENT_ROOT'] . '/Resources/PHP/model/DBManager/DB_Connection.php');
-
+include_once($_SERVER['DOCUMENT_ROOT'] . '/Resources/PHP/model/DBManager/DB_Connection.php');
 
 class DB_Instructor extends DB_Connection
 {
 
-    public function getInstructor(){
+    public function getInstructor()
+    {
         $this->getConnection();
 
         try {
             $this->dbc->beginTransaction();
-            $sth = $this->dbc->prepare('SELECT name, vorname, role, location, imgPath, content FROM recordbook.instructor');
+            $sth = $this->dbc->prepare('SELECT instructor_id, name, vorname, role, location, imgPath, content FROM recordbook.instructor');
 
             $sth->execute();
             $record = $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -21,9 +21,6 @@ class DB_Instructor extends DB_Connection
             return $record;
 
             $this->dbc->commit();
-
-
-
         } catch (PDOException $exception) {
             $this->dbc->rollBack();
             echo('[Error ] ' . $exception->getMessage() . 'in Line: ' . $exception->getLine());
@@ -33,10 +30,47 @@ class DB_Instructor extends DB_Connection
         return true;
     }
 
-    public function writeInstructor($name, $vorname, $location, $role, $content){
+    public function getSingleInstructor($single_id)
+    {
         $this->getConnection();
 
-        try{
+        try {
+            $this->dbc->beginTransaction();
+            $sth = $this->dbc->prepare(
+                'SELECT  name, vorname, role, location, imgPath, content FROM recordbook.instructor WHERE instructor_id = ?');
+
+            $sth->bindParam(1, $single_id, PDO::PARAM_INT);
+            $sth->execute();
+
+            $record = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+            $this->dbc->commit();
+
+            return $record;
+
+
+        } catch (PDOException $exception) {
+            $this->dbc->rollBack();
+            echo('[Error ] ' . $exception->getMessage() . 'in Line: ' . $exception->getLine());
+            return false;
+        }
+    }
+
+    public function writeInstructor(
+        $name,
+        $vorname = null,
+        $location = null,
+        $role = null,
+        $content = null
+    ) {
+        $this->getConnection();
+
+        if (isset($name) == false) {
+            echo '[Error] Kein Name gestetzt';
+            return false;
+        }
+
+        try {
             $this->dbc->beginTransaction();
             $sth = $this->dbc->prepare("INSERT INTO instructor (name, vorname, location, role, content) VALUES (?, ?, ?, ?, ?)");
 
@@ -50,9 +84,42 @@ class DB_Instructor extends DB_Connection
             $this->dbc->commit();
 
             return true;
+        } catch (PDOException $exception) {
+            $this->dbc->rollBack();
+            echo('[Error ] ' . $exception->getMessage() . 'in Line: ' . $exception->getLine());
+            return false;
+        }
+    }
 
+    public function updateInstructor(
+        $instructor_id,
+        $name = null,
+        $vorname = null,
+        $location = null,
+        $role = null,
+        $imgPath = null,
+        $content = null
+    ) {
+        $this->getConnection();
 
-        }catch(PDOException $exception){
+        try {
+            $this->dbc->beginTransaction();
+
+            $sth = $this->dbc->prepare("UPDATE instructor SET name=?, vorname=?, location=?, role=?, imgPath=?, content=? WHERE instructor_id=?");
+
+            $sth->bindParam(1, $name, PDO::PARAM_STR);
+            $sth->bindParam(2, $vorname, PDO::PARAM_STR);
+            $sth->bindParam(3, $location, PDO::PARAM_STR);
+            $sth->bindParam(4, $role, PDO::PARAM_STR);
+            $sth->bindParam(5, $imgPath, PDO::PARAM_STR);
+            $sth->bindParam(6, $content, PDO::PARAM_STR);
+            $sth->bindParam(7, $instructor_id, PDO::PARAM_INT);
+
+            $sth->execute();
+            $this->dbc->commit();
+
+            return true;
+        } catch (PDOException $exception) {
             $this->dbc->rollBack();
             echo('[Error ] ' . $exception->getMessage() . 'in Line: ' . $exception->getLine());
             return false;
